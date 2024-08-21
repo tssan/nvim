ea4cac1c39db11ddbd61944c72f15705f4ce926b
tssan <piotr.karasinski@gmail.com>
2024-08-22 00:09:00 +0200

    initial commit

---
+++ lua/options.lua
@@ -0,0 +1,19 @@
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+vim.opt.mouse = 'a'
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
