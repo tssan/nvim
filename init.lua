83e7b41559fd8a6165c07653e525a5411c8b0222
tssan <piotr.karasinski@gmail.com>
2024-08-22 11:45:11 +0200

    keymaps + colorscheme

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
 require('options')
 require('keymaps')
+require('plugins')
+require('colorscheme')
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,8 +1,12 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>l", vim.cmd.Ex)
+
 vim.keymap.set("n", "<leader><Up>", "<C-w>k")
 vim.keymap.set("n", "<leader><Down>", "<C-w>j")
 vim.keymap.set("n", "<leader><Left>", "<C-w>h")
 vim.keymap.set("n", "<leader><Right>", "<C-w>l")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
